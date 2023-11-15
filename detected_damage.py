1c436d02e6e8b8b6052c368ed70cb4f4739d942c
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2023-11-15 16:34:15 +0100

    refactoring: clean code

--- detected_damage.py
+++ detected_damage.py
@@ -26,25 +26,33 @@ class DetectedDamage:
     def save_new_training_data(self, detection_rating):
         original_image = Image.open(self.image_path)
 
-        # Calculate the bounding box coordinates with a 10px margin
-        margin = 10
-        left = int(self.x1 * original_image.width) - margin
-        upper = int(self.y1 * original_image.height) - margin
-        right = int(self.x2 * original_image.width) + margin
-        lower = int(self.y2 * original_image.height) + margin
+        left, lower, right, upper = self.calculate_bounding_box_coords(original_image)
 
         # Crop the bounding box region
         cropped_image = original_image.crop((left, upper, right, lower))
 
-        # Save the cropped image with a new filename
         base_path, ext = os.path.splitext(os.path.basename(self.image_path))
-        new_filename = os.path.join(self.TRAINING_IMAGES_PATH, f"{base_path}_{self.number}.png")
-        cropped_image.save(new_filename)
+        self.save_cropped_image(cropped_image, base_path)
 
-        # Save the detection information
+        self.save_label_file(base_path, detection_rating)
+
+    def save_label_file(self, base_path, detection_rating):
         detection_info = f"{detection_rating.value} {self.x1} {self.y1} {self.x2} {self.y2}"
         label_filename = os.path.join(self.TRAINING_LABELS_PATH, f"{base_path}_{self.number}.txt")
         with open(label_filename, 'a') as label_file:
             # only write coords to file if it is TP, create training label file in any case.
             if detection_rating == DetectionRating.TRUE_POSITIVE:
                 label_file.write(detection_info)
+
+    def save_cropped_image(self, cropped_image, base_path):
+        new_filename = os.path.join(self.TRAINING_IMAGES_PATH, f"{base_path}_{self.number}.png")
+        cropped_image.save(new_filename)
+
+    def calculate_bounding_box_coords(self, original_image):
+        # margin so only part of the image is labeled and some of the background is visible for training purposes
+        margin = 10
+        left = int(self.x1 * original_image.width) - margin
+        upper = int(self.y1 * original_image.height) - margin
+        right = int(self.x2 * original_image.width) + margin
+        lower = int(self.y2 * original_image.height) + margin
+        return left, lower, right, upper
